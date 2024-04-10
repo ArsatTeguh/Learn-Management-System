896f61a51114b9063cda266e47317db75af94087
arsyatteguh <arsyatteguh@gmail.com>
2024-04-10 11:06:59 +0700

    resolved

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: false, // Disable source maps in development
-  optimizeFonts: false, // Disable font optimization
-  images: {
-    unoptimized: true,
-  },
 };
   
   module.exports = nextConfig;
